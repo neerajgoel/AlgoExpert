e0e1e0133de71f9e0895c019421ef2e451c11deb
neeraj <neerajgoel.cbs@gmail.com>
2022-08-16 23:14:55 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import VeryHard.AirportConnections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AirportConnections.main(null);
+    }
+
+}
---
+++ src/VeryHard/AirportConnections.java
@@ -0,0 +1,9 @@
+package VeryHard;
+
+public class AirportConnections {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+
+}
